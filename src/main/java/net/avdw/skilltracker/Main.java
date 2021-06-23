453d5b9e82dd5dff4095eebc88f96943aecbb80b
Andrew van der Westhuizen <avanderw@gmail.com>
2021-06-23 10:41:53 +0200

    Fix database not upgrading with latest changesets

--- src/main/java/net/avdw/skilltracker/Main.java
+++ src/main/java/net/avdw/skilltracker/Main.java
@@ -1,35 +1,20 @@
 package net.avdw.skilltracker;
 
-import com.google.inject.Key;
-import com.google.inject.name.Names;
 import com.j256.ormlite.support.ConnectionSource;
 import lombok.SneakyThrows;
-import net.avdw.database.LiquibaseRunner;
 import picocli.CommandLine;
 
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 public final class Main {
     private Main() {
     }
 
     @SneakyThrows
-    private static void createDatabaseIfNotExists() {
-        String jdbcUrl = GuiceFactory.INJECTOR.getInstance(Key.get(String.class, Names.named(PropertyName.JDBC_URL)));
-        LiquibaseRunner liquibaseRunner = GuiceFactory.INJECTOR.getInstance(LiquibaseRunner.class);
-
-        if (Files.size(Paths.get(jdbcUrl.replace("jdbc:sqlite:", ""))) == 0) {
-            liquibaseRunner.update();
-        }
-    }
-
-    @SneakyThrows
     public static void main(final String[] args) {
-        createDatabaseIfNotExists();
+        GuiceFactory.getInstance().create(DbIntegrity.class).init();
 
         CommandLine commandLine = new CommandLine(MainCli.class, GuiceFactory.getInstance());
         PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out, StandardCharsets.UTF_8));
